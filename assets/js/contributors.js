b4187c83fd6affdec9dcde9d1d24e4d848e5c055
Hans5958 <email5958.1@gmail.com>
2020-09-18 21:41:16 +0700

    Use data from GitHub, make it look better

---
+++ assets/js/contributors.js
@@ -0,0 +1,57 @@
+const run = async () => {
+	response = await fetch("https://api.github.com/repos/ScratchAddons/ScratchAddons/contributors")
+	contributors = await response.json()
+	humanContributors = contributors.filter(contributor => contributor.type === "User")
+
+	document.querySelector(".lds-ellipsis").hidden = true
+
+	humanContributors.forEach((contributor, index) => {
+		
+		let nameEl = document.createElement("p")
+		nameEl.className = "user-name"
+		nameEl.textContent = contributor.login
+
+		let contribCountEl = document.createElement("p")
+		contribCountEl.className = "contribution-count"
+		contribCountEl.textContent = `${contributor.contributions} contributions`
+
+		let iconEl
+		iconEl = document.createElement("img")
+		iconEl.className = "user-icon"
+		iconEl.src = contributor.avatar_url
+
+		let iconWrap = document.createElement("div")
+		iconWrap.className = "icon-wrap"
+		iconWrap.appendChild(iconEl)
+
+		let infoWrap = document.createElement("div")
+		infoWrap.className = "info-wrap"
+		infoWrap.appendChild(nameEl)
+		infoWrap.appendChild(contribCountEl)
+
+		let linkEl = document.createElement("a")
+		linkEl.target = "_blank"
+		linkEl.href = `https://github.com/${contributor.login}`
+		linkEl.appendChild(iconWrap)
+		linkEl.appendChild(infoWrap)
+
+		let wrapEl = document.createElement("div")
+		wrapEl.className = "contributor"
+		wrapEl.appendChild(linkEl)
+		
+		let colEl = document.createElement("div")
+		colEl.className = "col-12 col-sm-6 col-md-4 col-xl-3"
+		colEl.appendChild(wrapEl)
+
+		// if (value % 4 === 0) {
+		// 	let rowEl = document.createElement("div")
+		// 	rowEl.className = "row"
+		// 	document.querySelector("#account").appendChild(rowEl)
+		// }
+
+		document.querySelector("#contributors .row").appendChild(colEl)
+
+	});
+}
+
+run()
