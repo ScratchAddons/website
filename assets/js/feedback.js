da1f7aa5a557d06f109bd7d08189631cbfcc1bdb
World_Languages <mundofinky@gmail.com>
2020-09-23 19:41:58 -0300

    Post feedback as JSON

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -7,9 +7,9 @@ document.getElementById("send-feedback").onclick = async function() {
     sent = true;
     this.style.display = "none";
     document.getElementById("sending").style.display = "block";
-    const body = `Version: ${version ? version : "unknown"}\nUser agent: ${navigator.userAgent}\n\`\`\`${document.querySelector("textarea").value}\`\`\``;
+    const body = {version, userAgent: navigator.userAgent, language: navigator.language, content: document.querySelector("textarea").value};
     try {
-        const res = await fetch("https://scratchaddons-feedback.glitch.me/send", {method:"POST", body});
+        const res = await fetch("https://scratchaddons-feedback.glitch.me/send", {method:"POST", body: JSON.stringify(body)});
         if (!res.ok) throw "";
         document.getElementById("thanks").style.display = "block";
         document.getElementsByTagName("textarea")[0].setAttribute("readonly", "true"); 
