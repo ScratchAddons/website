5a7d8433d2ca94e55c6d13be9e8ab663a895070a
Hans5958 <email5958.1@gmail.com>
2020-09-27 00:18:26 +0700

    Redesign feedback page

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,27 +1,41 @@
 fetch("https://scratchaddons-feedback.glitch.me/", {mode:'no-cors'})
-const version = new URL(location.href).searchParams.get("version");
-let sent = false;
+const version = new URL(location.href).searchParams.get("version")
 
-document.getElementById("send-feedback").onclick = async function() {
-    if (sent || document.querySelector("textarea").value.length <= 2) return;
-    sent = true;
-    this.style.display = "none";
-    document.getElementById("sending").style.display = "block";
+const setStatus = (statusText, status) => {
+    const element = document.querySelector('#feedback-status')
+    element.textContent = statusText
+    element.hidden = false
+    element.classList.remove([...element.classList].filter(className => /alert-/.exec(className))[0])
+    element.classList.add(`alert-${status}`)
+}
 
-    const body = {version, userAgent: navigator.userAgent, language: navigator.language, content: document.getElementById('mainfeedback').value, username: document.getElementById('username').value };
-    try {
-        const res = await fetch("https://scratchaddons-feedback.glitch.me/send", {method:"POST", body: JSON.stringify(body)});
-        if (!res.ok) throw "";
-        document.getElementById("thanks").style.display = "block";
-        document.getElementById("mainfeedback").setAttribute("readonly", "true"); 
-        document.getElementById("username").setAttribute("readonly", "true"); 
+document.querySelector("#feedback-submit").onclick = async function() {
+    document.querySelector('#feedback-username').readonly = true
+    document.querySelector("#feedback-content").readonly = true
+    document.querySelector("#feedback-submit").disabled = true
 
-    } catch(err) {
-        alert("Error sending feedback! Try again?");
-        sent = false;
-        this.style.display = "block";
+    // document.querySelector("#sending").style.display = "block";
+
+    const body = {
+        version, 
+        userAgent: navigator.userAgent, 
+        language: navigator.language, 
+        content: document.querySelector('#feedback-content').value, 
+        username: document.querySelector('#feedback-username').value 
     }
-    document.getElementById("sending").style.display = "none";
+
+    setStatus("Sending...", "primary")
+    try {
+        const res = await fetch("https://scratchaddons-feedback.glitch.me/send", {method:"POST", body: JSON.stringify(body)})
+        if (!res.ok) throw "";
+        setTimeout(() => document.querySelector("#feedback-submit").disabled = false, 10000)
+        setStatus("Sent! Thanks for the feedback", "success")
+    } catch(err) {
+        setStatus("Error sending feedback! Try again?", "danger")
+        document.querySelector("#feedback-submit").disabled = false
+    }
+    document.querySelector('#feedback-username').readonly = false
+    document.querySelector("#feedback-content").readonly = false
 };
 
 window.addEventListener("load", () => document.querySelector("textarea").focus());
