9e1ed5ad6ad4b3ff6c386e7526b15247773f5f26
Hans5958 <email5958.1@gmail.com>
2020-09-27 18:28:01 +0700

    Add extension-styled theme (and other minor style changes)
    (press SHIFT while clicking dark theme switch)

--- assets/js/website-base.js
+++ assets/js/website-base.js
@@ -26,7 +26,6 @@ window.addEventListener('load', () => {
 
 	const url = storeLinks[browser]
 
-	document.querySelector("#install-nav").classList.add(`install-${browser}`)
 	if (document.querySelector("#install-intro")) {
 		switch (browser) {
 			case "chrome":
@@ -46,6 +45,7 @@ window.addEventListener('load', () => {
 
 	for (const element of document.querySelectorAll(".install-btn")) {
 		element.href = url
+		element.classList.add(`install-${browser}`)
 		if (location.pathname !== "/" && browser !== "unsupported") element.target = "_blank"
 	}
 
@@ -58,10 +58,30 @@ window.addEventListener('load', () => {
 window.addEventListener('load', () => {
 
 	let darkTheme = false
+	let extensionStyledTheme = false
 	let toggle = document.querySelector("#dark-toggle")
-	
-	const update = () => {
+	let soraAdded = false
+
+	const updateExtensionStyledTheme = () => {
+		document.body.classList.toggle("extension-styled")
+		extensionStyledTheme = !extensionStyledTheme
+		localStorage.setItem("extensionStyledTheme", extensionStyledTheme)
+		if (!soraAdded) {
+			soraAdded = true
+			var linkEl = document.createElement('link')
+			linkEl.href = "https://fonts.googleapis.com/css2?family=Sora&amp;display=swap"
+			linkEl.rel = "stylesheet"
+			document.head.appendChild(linkEl)
+		}
+	}
+
+	const updateDarkTheme = (shiftPressed = false) => {
 		document.body.classList.toggle("no-animation")
+
+		if (shiftPressed || [...document.body.classList].indexOf(".extension-styled") + 1) {
+			updateExtensionStyledTheme()
+		} 
+
 		document.body.classList.toggle("dark")
 		setTimeout(() => document.body.classList.toggle("no-animation"), 200)
 		darkTheme = !darkTheme
@@ -78,17 +98,44 @@ window.addEventListener('load', () => {
 	} else {
 		toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:moon" data-inline="false"></span>'
 	}
+
+	if (localStorage.getItem("extensionStyledTheme")) {
+		updateExtensionStyledTheme()
+	} else {
+		localStorage.setItem("extensionStyledTheme", false)
+	}
 	
 	if (localStorage.getItem("darkTheme")) {
-		if (localStorage.getItem("darkTheme") == "true") {
-			update()
-		}
+		updateDarkTheme()
 	} else {
 		localStorage.setItem("darkTheme", false)
 	}	
 	
-	toggle.addEventListener("click", () => {
-		update()
+	toggle.addEventListener("click", event => {
+		if (event && event.shiftKey) updateExtensionStyledTheme()
+		updateDarkTheme()
 	})
 
+})
+
+/* =============================================================
+                       CONSOLE EASTER EGG
+============================================================= */
+
+window.addEventListener('load', () => {
+	console.log(
+`%cHello, console log readers!
+%c
+It seems that you wanted to know what happened behind the scenes. Well, there is not much happening here because the is quite simple.
+This site is built using Bootstrap 4 as the main theme, Jekyll as the site generator, and GitHub Pages as the hosting provider. Also, this site has been compressed before it reaches to you, so if you want to see the uncompressed source code, then go to https://github.com/ScratchAddons/website.
+
+If you wanted to report issues or wanted to suggest something about the website, please create an issue on https://github.com/ScratchAddons/website/issues (GitHub account required) or tell us on https://scratchaddons.com/feedback.
+
+We thank you for visiting the website, and enjoy using the extension if you do!
+-Hans5958
+
+%cBy the way, here's a little secret: You can toggle the similar style to the extension if you click the dark theme switch button while holding SHIFT. Keep in mind that the secret theme is made seperate because the current theme minds readability and accessibility better than the secret.`, 
+	"font-size: 1.5rem; font-weight: 500", 
+	"",
+	"font-style: italic; font-size: 0.75rem")
 })
