75b6b0fe9e381445cb4639c3da9ff8559e60304b
Hans5958 <email5958.1@gmail.com>
2021-03-14 22:39:10 +0700

    Another landing/index page redesign

---
+++ site/assets/js/landing.js
@@ -0,0 +1,24 @@
+import YTPlayer from 'https://cdn.skypack.dev/yt-player'
+
+let player, playerLoaded
+
+$('#modal-presentation').on('hide.bs.modal', () => {
+    player.stop()
+})
+
+$('#modal-presentation').on('shown.bs.modal', () => {
+	if (!playerLoaded) {
+		player.load("nQ4N9zlzj0M", true)
+		playerLoaded = true
+	}
+	player.play()
+})
+
+$(() => {
+	if (!player) player = new YTPlayer('.youtube-iframe', {
+        width: "1280",
+		height: "720",
+		related: 0,
+		host: "https://www.youtube-nocookie.com"
+    })
+})
